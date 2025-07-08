4c3f8c60629430f6e8d135c4467f1078ac1d3c2d
H. Ryott Glayzer <ryott.glayzer@mines.sdsmt.edu>
2025-07-08 10:29:52 -0600

    Fixed syntax error in startServer.sh

--- app/startServer.sh
+++ app/startServer.sh
@@ -5,4 +5,4 @@
 # Copyright © 2025 H. Ryott Glayzer
 
 echo -e "\033[1;32m Starting Uvicorn Server with debug level TRACE \033[0m"
-uvicorn main%app --reload --host 0.0.0.0 --port 8000 --log-level trace
+uvicorn main:app --reload --host 0.0.0.0 --port 8000 --log-level trace
