4ed97865d83e94544cdaa19ff7e31e4a2a79c2fe
H. Ryott Glayzer <ryott.glayzer@mines.sdsmt.edu>
2025-07-08 10:28:52 -0600

    Added bash script to start server

---
+++ app/startServer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Starts server for Rapid Ride RTS
+# MIT License
+# Copyright © 2025 H. Ryott Glayzer
+
+echo -e "\033[1;32m Starting Uvicorn Server with debug level TRACE \033[0m"
+uvicorn main%app --reload --host 0.0.0.0 --port 8000 --log-level trace
